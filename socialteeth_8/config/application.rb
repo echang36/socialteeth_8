57110ea01b7b0f49a885187f09894c134c64148f
Elaine Chang <elainefchang@gmail.com>
2012-04-24 00:12:54 -0700

    fixed devise bug by changing include js from :defaults to application in layouts page. Also added a config in config/application.rb

--- socialteeth_8/config/application.rb
+++ socialteeth_8/config/application.rb
@@ -44,5 +44,7 @@ module Socialteeth8
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+	
+	config.action_view.javascript_expansions[:defaults] = %w(jquery.min jquery_ujs)
   end
 end

ba6d9fa117890ac0e330c9a05158e4dd7e015e45
Elaine Chang <elainefchang@gmail.com>
2012-04-25 15:48:56 -0700

    Added some validation

--- socialteeth_8/app/models/ad.rb
+++ socialteeth_8/app/models/ad.rb
@@ -1,11 +1,18 @@
 class Ad < ActiveRecord::Base
   attr_accessible :title, :user, :goal, :format
   
+  
+  #RELATIONS
   belongs_to :user, :dependent => :destroy
   has_many :contributions
-  
-  validates_inclusion_of :format, :in=>[0,1,2], :allow_nil => false
-  
   has_attached_file :image
+  
+  #VALIDATIONS
+  
+  validates :title, :user, :goal, :format, presence: true
+  validates :title, uniqueness: true
+  validates_inclusion_of :format, :in=>[0,1,2], :allow_nil => false
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
 
 end

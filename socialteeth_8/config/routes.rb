0e7dfa3309abd6e89bc0b563466f2f477f3bd11a
Elaine Chang <elainefchang@gmail.com>
2012-04-27 12:05:12 -0700

    fixed page layout, added a home page

--- socialteeth_8/config/routes.rb
+++ socialteeth_8/config/routes.rb
@@ -56,7 +56,7 @@ Socialteeth8::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home/index', as: 'home'
+  root :to => 'home#index', as: 'home'
 
   # See how all your routes lay out with "rake routes"
 

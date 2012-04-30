a2baf5404048ea9c3555624a2a0649ca114be535
Elaine Chang <elainefchang@gmail.com>
2012-04-29 21:05:58 -0700

    rolled back paperclip

--- socialteeth_8/db/schema.rb
+++ socialteeth_8/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120425050523) do
+ActiveRecord::Schema.define(:version => 20120425012811) do
 
   create_table "ads", :force => true do |t|
     t.string   "title"
@@ -20,10 +20,6 @@ ActiveRecord::Schema.define(:version => 20120425050523) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "format"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "contributions", :force => true do |t|

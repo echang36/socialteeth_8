cd22043861a38edd2bf97a7dcfd7210150f8e143
Elaine Chang <elainefchang@gmail.com>
2012-05-01 01:39:07 -0700

    introduced thumbnails and show sizes on carrierwave, did some CSS for display boxes

--- socialteeth_8/app/uploaders/image_uploader.rb
+++ socialteeth_8/app/uploaders/image_uploader.rb
@@ -29,10 +29,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-     process :resize_to_limit => [300, 300]
+   version :thumb do
+     process :resize_to_limit => [200, 200]
    end
 
+   version :show do
+	 process :resize_to_limit => [500, 500]
+   end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
